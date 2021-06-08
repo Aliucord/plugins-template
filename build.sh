3686abe5bd9418b12f2a4e7478ee6be2bdccd5b7
Kyza <kyza@kyza.net>
2021-06-08 18:57:33 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -ex
 cd ../buildtool
 ./main -p "$1"
 cd ./buildsPlugins
-adb push "./${1}.apk" /storage/emulated/0/Aliucord/plugins
+adb push "./${1}.zip" /storage/emulated/0/Aliucord/plugins
 adb shell am force-stop com.aliucord
 adb shell monkey -p com.aliucord -c android.intent.category.LAUNCHER 1
